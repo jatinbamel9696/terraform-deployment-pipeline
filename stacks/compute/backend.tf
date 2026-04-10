952f9e6be31fe9598499df59c01d8d26b5d2f313
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2026-04-10 16:17:22 +0530

    updated

--- stacks/compute/backend.tf
+++ stacks/compute/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket         = "s3-backend-git-9696"
     # Use placeholder - will be set at runtime via -backend-config
     # key format: stacks/compute/REGION/terraform.tfstate
+    # region: always us-east-1 (where bucket exists)
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
